eb2838657b640d8fb6d73260cb417c356b04863c
Svitlana Zaster <szaster@protonmail.com>
2020-08-24 16:47:05 -0500

    added jumbotron image

--- react-employee-directory-app/src/components/PageHeader/index.js
+++ react-employee-directory-app/src/components/PageHeader/index.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./style.css";
 
 function PageHeader() {
   return (
-    <div className="jumbotron jumbotron-fluid">
+    <div className="jumbotron jumbotron-fluid text-white">
       <div className="container">
         <h1>Employee Directory</h1>
         <h4>Click on the arrows to sort the employee list</h4>
